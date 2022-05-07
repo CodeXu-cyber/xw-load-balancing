af8be942aa6604b87559ebed4cd84a6536920ee7
徐玮 <xuwei@xuweideMacBook-Pro.local>
2022-05-07 22:55:20 +0800

    修改配置文件解析

--- src/main/java/system/configure/Configuration.java
+++ src/main/java/system/configure/Configuration.java
@@ -1,13 +1,16 @@
 package system.configure;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import system.entity.Server;
 import system.random.BalanceService;
 import system.random.imp.*;
-import utils.XmlUtil;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @ClassName Configuration
@@ -16,7 +19,6 @@ import java.util.Map;
  */
 public class Configuration {
     private volatile static Configuration configuration;
-    private List<Server> serverList;
     private BalanceService balanceService;
     private Integer port;
 
@@ -34,27 +36,58 @@ public class Configuration {
     private Configuration(String fileName) {
         File file = new File(fileName);
         if (file.exists()) {
-            Map<String, Object> config = XmlUtil.analysisConfig(file);
-            this.port = Integer.valueOf(config.get("port") == null ? "8088" : "".equals(config.get("port")) ? "8088" : (String) config.get("port"));
-            this.serverList = (List<Server>) config.get("servers");
-            String random = config.get("random") == null ? "RandomServer" : "".equals(config.get("random")) ? "RandomServer" : (String) config.get("random");
-            switch (random) {
-                case "WeightRandomServer":
-                    balanceService = new WeightRandomServerImpl(serverList);
-                    break;
-                case "PollServer":
-                    balanceService = new PollServerImpl(serverList);
-                    break;
-                case "WeightPollServer":
-                    balanceService = new WeightPollServerImpl(serverList);
-                    break;
-                case "HashServer":
-                    balanceService = new HashServerImpl(serverList);
-                    break;
-                case "RandomServer":
-                default:
-                    balanceService = new RandomServerImpl(serverList);
-                    break;
+            SAXReader reader = new SAXReader();
+            List<Server> serverList = new ArrayList<>();
+            Document document = null;
+            try {
+                document = reader.read(file);
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            }
+            assert document != null;
+            Element root = document.getRootElement();
+            List<Element> childElements = root.elements();
+            for (Element child : childElements) {
+                if (!child.elements().isEmpty()) {
+                    for (Element c : child.elements()) {
+                        switch (child.getName()) {
+                            case "servers":
+                                serverList.add(new Server(c.attributeValue("name"), c.attributeValue("address"), Integer.valueOf("".equals(c.attributeValue("port")) ? "80" : c.attributeValue("port")), Integer.valueOf("".equals(c.attributeValue("weight")) ? "0" : c.attributeValue("weight"))));
+                                break;
+                            case "settings":
+                                switch (c.attributeValue("name")) {
+                                    case "port":
+                                        this.port = Integer.valueOf(c.attributeValue("value") == null ? "8088" : "".equals(c.attributeValue("value")) ? "8088" : c.attributeValue("value"));
+                                        break;
+                                    case "random":
+                                        String random = c.attributeValue("value") == null ? "RandomServer" : "".equals(c.attributeValue("value")) ? "RandomServer" : c.attributeValue("value");
+                                        switch (random) {
+                                            case "WeightRandomServer":
+                                                balanceService = new WeightRandomServerImpl(serverList);
+                                                break;
+                                            case "PollServer":
+                                                balanceService = new PollServerImpl(serverList);
+                                                break;
+                                            case "WeightPollServer":
+                                                balanceService = new WeightPollServerImpl(serverList);
+                                                break;
+                                            case "HashServer":
+                                                balanceService = new HashServerImpl(serverList);
+                                                break;
+                                            case "RandomServer":
+                                            default:
+                                                balanceService = new RandomServerImpl(serverList);
+                                                break;
+                                        }
+                                    default:
+                                        break;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
             }
         }
     }
